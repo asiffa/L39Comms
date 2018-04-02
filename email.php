75eb3f334837b6155d77d2e52580f711323495b9
Asif <asif.fa@gmail.com>
2018-04-02 20:32:59 +0100

    Initial files

---
+++ email.php
@@ -0,0 +1,12 @@
+
+<html>
+<body>
+    
+Welcome <?php echo $_POST["username"]; ?><br>
+Your email address is: <?php echo $_POST["email_address"]; ?><br>
+
+Your message is: <?php echo $_POST["message"]; ?>
+    
+</body>
+</html>
+
